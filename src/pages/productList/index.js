7641e90274b82a57aed41b3c26427f45fe1cded0
thankstiger <474833426@qq.com>
2019-10-04 12:16:20 +0800

    项目案例列表和详情

---
+++ src/pages/productList/index.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'antd'
+import Link from 'umi/link';
+import styles from './index.less'
+
+import productListCover from '../../assets/productList_cover.png'
+import productCover1 from '../../assets/productListItem1.png'
+import productCover2 from '../../assets/productListItem2.png'
+import productCover3 from '../../assets/productListItem3.png'
+import productCover4 from '../../assets/productListItem4.png'
+
+const productListItem = [
+  {
+    avatar: productCover1,
+    title: '湖南省最高规格的金融盛会，荣获腾讯2017年度影响力传播奖',
+  },
+  {
+    avatar: productCover2,
+    title: '青少年“体育·新时代”冬夏令营系列活动',
+  },
+  {
+    avatar: productCover3,
+    title: '谱写新时代中国饭店业高质量发展新篇章',
+  },
+  {
+    avatar: productCover4,
+    title: '湘江西岸建设一座美好的“两型”新城',
+  },
+  {
+    avatar: productCover1,
+    title: '湘江新区庆“七一”支部党建成果汇报会圆满落地',
+  },
+  {
+    avatar: productCover2,
+    title: '湖南金融中心首次向全省全市人民亮出了金质名片',
+  },
+  {
+    avatar: productCover3,
+    title: '凯德壹中心商业综合体外立面广告位',
+  },
+  {
+    avatar: productCover4,
+    title: '洋湖体育公园导视系统工程',
+  }
+]
+
+export default class Product extends Component {
+  render () {
+    return (
+      <div>
+        <img className={styles.productListCover} src={productListCover} alt='' />
+        <div className={styles.productContainer}>
+          <div className={styles.productListShow}>
+            <h2>优秀案例</h2>
+            <p>精心打造各种品牌活动，为客户提供对外展示的窗口</p>
+          </div>
+          <div>
+          <Row>
+            {
+              productListItem.map((item, index) => {
+                return (
+                  <Col xs={24} sm={24} md={12} lg={12}>
+                    <div className={styles.productListItem}>
+                      <img src={item.avatar} alt='' />
+                      <Link to='./productDetail'>{item.title}</Link>
+                    </div>
+                  </Col>
+                )
+              })
+            }
+          </Row>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
